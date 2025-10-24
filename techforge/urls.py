50559d4da14be0cdf5386302d39dce58fe2730e5
techforge <freemantitus0@gmail.com>
2025-10-24 17:52:46 +0100

    update index

--- techforge/urls.py
+++ techforge/urls.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
 from django.urls import path
 from core import views
 
@@ -9,3 +11,5 @@ urlpatterns = [
     path('contact/', views.contact_view, name='contact'),
 
 ]
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
